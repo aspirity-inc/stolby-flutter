bdf41c539c43dd92bcbe32566ee157e15a244e3a
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-11 17:33:57 +0700

    KS-92/Added rocks list

--- lib/infrastructure/services/local/database/app_database.dart
+++ lib/infrastructure/services/local/database/app_database.dart
@@ -35,7 +35,7 @@ class AppDatabase extends _$AppDatabase {
         ),
       ],
     )..where(
-        rocksLocalized.language.like(language),
+        rocksLocalized.language.equals(language),
       );
     final result = await query.get();
 
