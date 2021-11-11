ab4565ae17399582cfe373bebe693eee00a267f0
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-11 13:42:26 +0700

    KS-91/database initialization

--- lib/infrastructure/services/local/database/rocks_database.dart
+++ lib/infrastructure/services/local/database/rocks_database.dart
@@ -3,8 +3,8 @@ import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
-import 'package:stolby_flutter/infrastructure/services/local/rocks_database/tables/rocks.dart';
-import 'package:stolby_flutter/infrastructure/services/local/rocks_database/tables/rocks_localized.dart';
+import 'package:stolby_flutter/infrastructure/services/local/database/tables/rocks.dart';
+import 'package:stolby_flutter/infrastructure/services/local/database/tables/rocks_localized.dart';
 
 part 'rocks_database.g.dart';
 
