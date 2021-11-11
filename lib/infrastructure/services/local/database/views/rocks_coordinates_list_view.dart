5057033cccb42af7b4667d63d1a89d1e39b203f0
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-11 17:28:33 +0700

    KS-93/Added rocks coordinates sql query

---
+++ lib/infrastructure/services/local/database/views/rocks_coordinates_list_view.dart
@@ -0,0 +1,15 @@
+class RocksCoordinatesListView {
+  final int id;
+  final double latitude;
+  final double longitude;
+  final String picName;
+  final String localizedName;
+
+  RocksCoordinatesListView({
+    required this.id,
+    required this.latitude,
+    required this.longitude,
+    required this.picName,
+    required this.localizedName,
+  });
+}
