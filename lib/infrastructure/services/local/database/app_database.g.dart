fd11e97475b6ef611f89d2dbf07d2cc182afea2c
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-11 17:07:54 +0700

    KS-92/Added rocks list with localization

--- lib/infrastructure/services/local/database/app_database.g.dart
+++ lib/infrastructure/services/local/database/app_database.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'rocks_database.dart';
+part of 'app_database.dart';
 
 // **************************************************************************
 // MoorGenerator
