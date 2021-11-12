dbae589dd03d7ff7210366a8225e374273924e3d
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-12 12:49:34 +0700

    KS-95 entities and repositories

--- lib/domain/feature/rocks_map/i_map_repository.dart
+++ lib/domain/feature/rocks_map/i_map_repository.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:stolby_flutter/domain/core/failures.dart';
 import 'package:stolby_flutter/domain/feature/rocks_map/entities/rock_map_entity.dart';
 
-abstract class IRocksMapRepository {
+abstract class IMapRepository {
   Future<Either<DatabaseFailure, List<RockMapEntity>>> getRocksCoordinatesList(
     String language,
   );
