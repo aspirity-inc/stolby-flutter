8d81b4f1dfab0bebdc4b6486449af9d0b1d201b8
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-17 13:23:09 +0700

    KS-105/shared_preferences_services

--- test/infrastructure/core/services_injectable_module.dart
+++ test/infrastructure/core/services_injectable_module.dart
@@ -4,6 +4,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 @module
 abstract class ServicesInjectionModule {
   @preResolve
+  @lazySingleton
   Future<SharedPreferences> get sharedPreferences =>
       SharedPreferences.getInstance();
 }
