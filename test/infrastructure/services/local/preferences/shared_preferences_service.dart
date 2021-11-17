e21a1932b373a576d57ae3487a310e64675d631c
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-17 13:37:19 +0700

    KS-105/shared_preferences_services

--- test/infrastructure/services/local/preferences/shared_preferences_service.dart
+++ test/infrastructure/services/local/preferences/shared_preferences_service.dart
@@ -9,25 +9,26 @@ class SharedPreferencesService {
 
   SharedPreferencesService(this._preferences);
 
-  bool? get geolocationEnabled =>
-      _preferences.getBool(PrefNames.geolocationEnabled);
+  bool get geolocationEnabled =>
+      _preferences.getBool(PrefNames.geolocationEnabled) ?? false;
   Future<void> setGeolocationEnabled(bool? geolocationEnabled) => _preferences
       .setBool(PrefNames.geolocationEnabled, geolocationEnabled ?? false);
 
-  bool? get reversedMap => _preferences.getBool(PrefNames.reversedMap);
+  bool? get reversedMap => _preferences.getBool(PrefNames.reversedMap) ?? false;
   Future<void> setReversedMap(bool? reversedMap) =>
       _preferences.setBool(PrefNames.reversedMap, reversedMap ?? false);
 
   bool? get mapUserCentering =>
-      _preferences.getBool(PrefNames.mapUserCentering);
+      _preferences.getBool(PrefNames.mapUserCentering) ?? false;
   Future<void> setMapUserCentering(bool? mapUserCentering) => _preferences
       .setBool(PrefNames.mapUserCentering, mapUserCentering ?? false);
 
-  bool? get autoThemeChange => _preferences.getBool(PrefNames.autoThemeChange);
+  bool? get autoThemeChange =>
+      _preferences.getBool(PrefNames.autoThemeChange) ?? false;
   Future<void> setAutoThemeChange(bool? autoThemeChange) =>
       _preferences.setBool(PrefNames.autoThemeChange, autoThemeChange ?? false);
 
-  bool? get lightTheme => _preferences.getBool(PrefNames.lightTheme);
+  bool? get lightTheme => _preferences.getBool(PrefNames.lightTheme) ?? false;
   Future<void> setLightTheme(bool? lightTheme) =>
       _preferences.setBool(PrefNames.lightTheme, lightTheme ?? false);
 }
