32714c3b599af1865f8448b651bbd178eca9a663
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-17 13:01:00 +0700

    KS-105/shared_preferences_services

---
+++ test/infrastructure/services/local/preferences/pref_names.dart
@@ -0,0 +1,7 @@
+class PrefNames {
+  static const String geolocationEnabled = "geolocationEnabled";
+  static const String reversedMap = "reversedMapEnabled";
+  static const String mapUserCentering = "mapUserCentering";
+  static const String autoThemeChange = "autoThemeChange";
+  static const String lightTheme = "lightTheme";
+}
---
+++ test/infrastructure/services/local/preferences/shared_preferences_service.dart
@@ -0,0 +1,33 @@
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'pref_names.dart';
+
+@lazySingleton
+class SharedPreferncesService {
+  final SharedPreferences _preferences;
+
+  SharedPreferncesService(this._preferences);
+
+  bool? get geolocationEnabled =>
+      _preferences.getBool(PrefNames.geolocationEnabled);
+  Future<void> setGeolocationEnabled(bool? geolocationEnabled) => _preferences
+      .setBool(PrefNames.geolocationEnabled, geolocationEnabled ?? false);
+
+  bool? get reversedMap => _preferences.getBool(PrefNames.reversedMap);
+  Future<void> setReversedMap(bool? reversedMap) =>
+      _preferences.setBool(PrefNames.reversedMap, reversedMap ?? false);
+
+  bool? get mapUserCentering =>
+      _preferences.getBool(PrefNames.mapUserCentering);
+  Future<void> setMapUserCentering(bool? mapUserCentering) => _preferences
+      .setBool(PrefNames.mapUserCentering, mapUserCentering ?? false);
+
+  bool? get autoThemeChange => _preferences.getBool(PrefNames.autoThemeChange);
+  Future<void> setAutoThemeChange(bool? autoThemeChange) =>
+      _preferences.setBool(PrefNames.autoThemeChange, autoThemeChange ?? false);
+
+  bool? get lightTheme => _preferences.getBool(PrefNames.lightTheme);
+  Future<void> setLightTheme(bool? lightTheme) =>
+      _preferences.setBool(PrefNames.lightTheme, lightTheme ?? false);
+}
