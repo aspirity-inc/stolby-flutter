5e2aab9ecb0682b922577f12e6d9028d5d753770
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-17 13:59:36 +0700

    KS-105/shared_preferences_services

--- test/infrastructure/services/local/preferences/shared_preferences_service.dart
+++ test/infrastructure/services/local/preferences/shared_preferences_service.dart
@@ -11,21 +11,22 @@ class SharedPreferencesService {
 
   bool get geolocationEnabled =>
       _preferences.getBool(PrefNames.geolocationEnabled) ?? false;
-  Future<void> setGeolocationEnabled(bool geolocationEnabled) =>
+  Future<void> setGeolocationEnabled({bool geolocationEnabled = false}) =>
       _preferences.setBool(
         PrefNames.geolocationEnabled,
         geolocationEnabled,
       );
 
   bool get reversedMap => _preferences.getBool(PrefNames.reversedMap) ?? false;
-  Future<void> setReversedMap(bool reversedMap) => _preferences.setBool(
+  Future<void> setReversedMap({bool reversedMap = false}) =>
+      _preferences.setBool(
         PrefNames.reversedMap,
         reversedMap,
       );
 
   bool get mapUserCentering =>
       _preferences.getBool(PrefNames.mapUserCentering) ?? false;
-  Future<void> setMapUserCentering(bool mapUserCentering) =>
+  Future<void> setMapUserCentering({bool mapUserCentering = false}) =>
       _preferences.setBool(
         PrefNames.mapUserCentering,
         mapUserCentering,
@@ -33,13 +34,14 @@ class SharedPreferencesService {
 
   bool get autoThemeChange =>
       _preferences.getBool(PrefNames.autoThemeChange) ?? false;
-  Future<void> setAutoThemeChange(bool autoThemeChange) => _preferences.setBool(
+  Future<void> setAutoThemeChange({bool autoThemeChange = false}) =>
+      _preferences.setBool(
         PrefNames.autoThemeChange,
         autoThemeChange,
       );
 
   bool get lightTheme => _preferences.getBool(PrefNames.lightTheme) ?? false;
-  Future<void> setLightTheme(bool lightTheme) => _preferences.setBool(
+  Future<void> setLightTheme({bool lightTheme = false}) => _preferences.setBool(
         PrefNames.lightTheme,
         lightTheme,
       );
