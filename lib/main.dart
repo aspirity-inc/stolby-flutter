d30456502afb42000bac6456d853c952ef06fada
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-19 17:26:10 +0700

    KS-111/android_geolocation

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:location/location.dart';
 import 'package:stolby_flutter/injection.dart';
 import 'package:stolby_flutter/presentation/core/app_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   configureInjection(Environment.dev);
   runApp(StolbyApp());
 }
