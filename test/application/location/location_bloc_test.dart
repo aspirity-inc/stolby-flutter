fc7f49b2250a7bba8e316ca5500c9ea08ffe0484
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-23 17:49:47 +0700

    KS-118/intermediate commit

---
+++ test/application/location/location_bloc_test.dart
@@ -0,0 +1,70 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:stolby_flutter/application/location/location_bloc.dart';
+import 'package:stolby_flutter/domain/core/failures.dart';
+import 'package:stolby_flutter/domain/feature/location/i_location_repository.dart';
+
+import 'location_bloc_test.mocks.dart';
+
+@GenerateMocks([ILocationRepository])
+void main() {
+  late MockILocationRepository _repository;
+  late LocationBloc _bloc;
+
+  setUp(() {
+    _repository = MockILocationRepository();
+    _bloc = LocationBloc(_repository);
+  });
+
+  tearDown(() => _bloc.close());
+
+  group("initial", () {
+    test("Should emit correct initial state", () {
+      final result = _bloc.state;
+      expect(result, LocationState.initial());
+    });
+  });
+
+  group("checkedPermission()", () {
+    blocTest(
+      "Should emit true if locationPermission is enable",
+      build: () {
+        when(_repository.getLocationPermissions())
+            .thenAnswer((_) async => right(unit));
+
+        return _bloc;
+      },
+      seed: () => LocationState.initial(),
+      act: (LocationBloc bloc) =>
+          bloc.add(const LocationEvent.checkedPermission()),
+      expect: () => [
+        LocationState.initial().copyWith(hasPermission: true),
+      ],
+    );
+
+    blocTest(
+      "Should emit LocationFailure.notGranted if permissions not granted",
+      build: () {
+        when(_repository.getLocationPermissions())
+            .thenAnswer((_) async => left(const LocationFailure.notGranted()));
+
+        return _bloc;
+      },
+    );
+
+    blocTest(
+      "Should emit LocationFailure.unexpected if unexpectedError happens",
+      build: () {
+        when(_repository.getLocationPermissions())
+            .thenAnswer((_) async => left(const LocationFailure.unexpected()));
+
+        return _bloc;
+      },
+    );
+  });
+  group("startWatchingLocation()", () {});
+  group("stopWatchingLocation()", () {});
+}
