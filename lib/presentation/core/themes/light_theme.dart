1c73f8c6b7ee4ddb9a5d891509e7b517fd60a62f
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-24 11:43:30 +0700

    KS-120/intermediate commit

---
+++ lib/presentation/core/themes/light_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+final ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  primaryColor: const Color(0xFF009688),
+  colorScheme: const ColorScheme.light().copyWith(
+    primary: Color(0xFF009688),
+  ),
+  scaffoldBackgroundColor: Colors.white,
+  accentColor:
+);
