aa2b2fc11983212b78f0f0d0f9cc40b396f62328
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-24 12:51:37 +0700

    KS-120/app_theme

--- lib/presentation/core/themes/app_colors.dart
+++ lib/presentation/core/themes/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color primaryLight = Color(0xFF009688);
-  static const Color primaryDark = Color(0xFF009688);
-  static const Color secondaryColor = Color(0xFF009688);
+  static const Color primaryDark = Color(0xFF161616);
+  static const Color secondaryColor = Color(0xFFF2994A);
   static const Color backgroundDark = Color(0xFF262626);
 }
