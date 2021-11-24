d297bc88b66327816a168a8ad4c3a7ddbd15e8b7
Mikhail Chernetsov <Mchernetsov@aspirity.com>
2021-11-24 10:54:24 +0700

    KS-119/provide_blocs_to_app

--- lib/presentation/map_page/map_page.dart
+++ lib/presentation/map_page/map_page.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:stolby_flutter/application/map/map_bloc.dart';
+import 'package:stolby_flutter/injection.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          color: Colors.red,
-          child: const Text('Map was not implemented'),
-          // TODO: implement map page
+    return BlocProvider(
+      create: (_) => getIt<MapBloc>()
+        ..add(
+          const MapEvent.initialized(),
+        ),
+      child: Scaffold(
+        body: Center(
+          child: Container(
+            width: 100,
+            height: 100,
+            color: Colors.red,
+            child: const Text('Map was not implemented'),
+            // TODO: implement map page
+          ),
         ),
       ),
     );
